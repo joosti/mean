213c0d3150871ada593307a43d9bde42e2084545
Joost <joost@joost-laptop.(none)>
2014-05-13 15:39:38 -0400

    allow module dependencies

--- public/config.js
+++ public/config.js
@@ -7,9 +7,10 @@ var ApplicationConfiguration = (function() {
 	var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap', 'ui.utils'];
 
 	// Add a new vertical module
-	var registerModule = function(moduleName) {
+	// @moduleDependencies: array of dependent modules, e.g. ['root']
+	var registerModule = function(moduleName, moduleDependencies) {
 		// Create angular module
-		angular.module(moduleName, []);
+		angular.module(moduleName, moduleDependencies || []);
 
 		// Add the module to the AngularJS configuration file
 		angular.module(applicationModuleName).requires.push(moduleName);
